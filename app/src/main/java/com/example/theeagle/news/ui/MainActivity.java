fac961ed1f6d057fcb8cb0db58943a21f2018afa
theeagle <ahmedkamar@gmail.com>
2018-05-21 15:19:57 +0200

    Add settings activity

--- app/src/main/java/com/example/theeagle/news/ui/MainActivity.java
+++ app/src/main/java/com/example/theeagle/news/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.theeagle.news.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
@@ -11,9 +12,12 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
 import com.example.theeagle.news.R;
 import com.example.theeagle.news.adapters.NewsFeedAdapter;
 import com.example.theeagle.news.constants.Constants;
@@ -32,6 +36,21 @@ public class MainActivity extends AppCompatActivity implements
     private ProgressBar progressBar;
     private RecyclerView recyclerView;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings_item:
+                startActivity(new Intent(this, SettingsActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
