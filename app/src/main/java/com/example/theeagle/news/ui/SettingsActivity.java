85015887c3bedab8e95969da8e222e34df295a38
theeagle <ahmedkamar@gmail.com>
2018-05-29 00:08:02 +0200

    Configure preference screen

--- app/src/main/java/com/example/theeagle/news/ui/SettingsActivity.java
+++ app/src/main/java/com/example/theeagle/news/ui/SettingsActivity.java
@@ -4,44 +4,23 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import com.example.theeagle.news.R;
 
-public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView categoriesTextView;
-    private PreferenceScreen preferenceScreen=new PreferenceScreen();
+public class SettingsActivity extends AppCompatActivity {
+    private PreferenceScreen preferenceScreen = new PreferenceScreen();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        initViews();
-        listeners();
-
-    }
-
-    private void listeners() {
-        categoriesTextView.setOnClickListener(this);
-    }
-
-    private void initViews() {
-        categoriesTextView = findViewById(R.id.categories);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.categories:
-                addFragment();
-                break;
-        }
+        addFragment();
     }
 
     private void addFragment() {
-        FragmentManager fragmentManager=getFragmentManager();
-        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(android.R.id.content, preferenceScreen);
-        fragmentTransaction.commit();    }
+        fragmentTransaction.commit();
+    }
 }
